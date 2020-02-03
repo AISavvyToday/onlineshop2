9e733452a6f96ad1378fb46c6c2de766db3f3ade
MuhuriJSON <muhuri.json@gmail.com>
2020-02-03 14:57:47 +0300

    login required checkout

--- orders/views.py
+++ orders/views.py
@@ -4,6 +4,7 @@ from carts.models import Cart
 from django.urls import reverse
 from . models import Order
 from .utils import id_generator
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
@@ -13,7 +14,7 @@ def Orders(request):
 	return render(request, 'user-orders.html/', context)
 
 
-
+@login_required
 def Checkout(request):
 	try:
 		the_id = request.session['cart_id']
@@ -21,12 +22,17 @@ def Checkout(request):
 	except:
 		the_id = None
 		return HttpResponseRedirect(reverse('cart'))
-	new_order, created = Order.objects.get_or_create(cart=cart)
-	if created:
+	try:
+		new_order = Order.objects.get(cart=cart)
+	except Order.DoesNotExist:
+		new_order = Order()
+		new_order.cart = cart
+		new_order.user = request.user
 		new_order.order_id = id_generator()
 		new_order.save()
-	new_order.user = request.user
-	new_order.save()
+	except:
+		return HttpResponseRedirect(reverse('cart'))
+
 	if new_order.status == 'Finished':
 		# cart.delete()
 		del request.session['cart_id']
