8d4484ef8019b31ae96c113e8599963ce45db4b9
MuhuriJSON <muhuri.json@gmail.com>
2020-02-05 09:05:14 +0300

    unique email field

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,69 @@
 from django import forms
+from django.contrib.auth import get_user_model
 
 
+User = get_user_model()
 
 class LogInForm(forms.Form):
 	username = forms.CharField()
 	password = forms.CharField(widget=forms.PasswordInput())
+
+	def clean_username(self):
+		username = self.cleaned_data.get('username')
+		try:
+			user = User.objects.get(username=username)
+		except User.DoesNotExist:
+			raise forms.ValidationError('User not found! Are you sure you are registered?Please go ahead and create an account')
+		return username
+
+	def clean_password(self):
+		username = self.cleaned_data.get('username')
+		password = self.cleaned_data.get('password')
+		try:
+			user = User.objects.get(username=username)
+		except:
+			user = None
+		if user is not None and not user.check_password(password):
+			raise forms.ValidationError('Invalid password! Please try again')
+		elif user is None:
+			pass
+		else:
+			return password
+
+
+class RegistrationForm(forms.ModelForm):
+	email = forms.EmailField(label='Email')
+	password1 = forms.CharField(label='Set Password', widget=forms.PasswordInput())
+	password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput())
+
+	class Meta:
+		model = User
+		fields = ['username','email']
+
+	def clean_password2(self):
+		password1 = self.cleaned_data.get('password1')
+		password2 = self.cleaned_data.get('password2')
+		if password1 and password2 and password1 != password2:
+			raise forms.ValidationError('Passwords do not match')
+		return password2
+
+	def clean_email(self):
+		email = self.cleaned_data.get('email')
+		email_count = User.objects.filter(email=email).count()
+		if email_count > 0:
+			raise forms.ValidationError('This email is already registered, Please check and try again or reset your password')
+		return email
+
+	def save(self, commit=True):
+		user = super(RegistrationForm, self).save(commit=False)
+		user.set_password(self.cleaned_data['password1'])
+		if commit:
+			user.save()
+		return user
+
+
+
+
+
+	
+		
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.contrib.auth import logout, login, authenticate
-from . forms import LogInForm
+from . forms import LogInForm, RegistrationForm
 
 # Create your views here.
 
@@ -20,5 +20,22 @@ def LogInView(request):
 		login(request, user)
 	context = {
 		'form': form
+	}
+	return render(request, 'form.html', context)
+
+
+
+def RegisterView(request):
+	form = RegistrationForm(request.POST or None)
+	if form.is_valid():
+		new_user = form.save(commit=False)
+		# new_user.first_name = 'Winnie' # customize model form
+		new_user.save()
+		# username = form.cleaned_data['username']
+		# password = form.cleaned_data['password']
+		# user = authenticate(username=username, password=password)
+		# login(request, user)
+	context = {
+	'form': form
 	}
 	return render(request, 'form.html', context)
